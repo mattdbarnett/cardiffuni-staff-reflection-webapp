c29d44af9422df194613446b4fded1f2fc93d2c6
Matt <barnettmd@cardiff.ac.uk>
2020-11-19 14:42:40 +0000

    Created the base-spring project files

---
+++ src/main/java/group03/project/Group03StaffProjectApplication.java
@@ -0,0 +1,13 @@
+package group03.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group03StaffProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group03StaffProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group03/project/Group03StaffProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package group03.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group03StaffProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
