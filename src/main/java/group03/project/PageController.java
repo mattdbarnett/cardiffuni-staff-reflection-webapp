6b2cdbba595f4db8bb61dd8c2737c41efc9b1a1e
Ronan Manning <ManningR4@cardiff.ac.uk>
2020-12-02 21:41:56 +0000

    Added some styling to the "my activities" page

--- src/main/java/group03/project/PageController.java
+++ src/main/java/group03/project/PageController.java
@@ -47,6 +47,13 @@ public class PageController {
         return model;
     }
 
+    @GetMapping("/my_activities")
+    public ModelAndView myactivities(){
+        ModelAndView model = new ModelAndView();
+        model.setViewName("My_activities");
+        return model;
+    }
+
 
     /*@RequestMapping(value="/login",method= RequestMethod.POST)
     public void loginUser(LoginRequest  ){
