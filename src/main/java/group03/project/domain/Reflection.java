8ffc09d6b4dba76b903f018d2e406d2efa3046af
Matt <barnettmd@cardiff.ac.uk>
2020-12-16 23:40:01 +0000

    Added validation for reflection class

--- src/main/java/group03/project/domain/Reflection.java
+++ src/main/java/group03/project/domain/Reflection.java
@@ -3,6 +3,9 @@ package group03.project.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
 
@@ -14,26 +17,45 @@ public class Reflection {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="reflectionID")
+    @NonNull
+    @Column(name="reflectionID", nullable = false)
     private Long reflectionID;
-    @Column(name="Participation_participationID")
+
+    @NonNull
+    @Column(name="Participation_participationID", nullable = false)
     private Long participationID;
+
     @Column(name="Tag_tagID")
     private Long tagID;
+
+    @Length(max = 180)
     @Column(name="Reflect_what")
     private String reflect_what;
+
+    @Length(max = 180)
     @Column(name="Reflect_prompt")
     private String reflect_prompt;
+
+    @Length(max = 180)
     @Column(name="Reflect_happen")
     private String reflect_happen;
+
+    @Length(max = 180)
     @Column(name="Reflect_eval")
     private String reflect_eval;
+
+    @Length(max = 180)
     @Column(name="Reflect_diff")
     private String reflect_diff;
+
+    @Length(max = 180)
     @Column(name="Reflect_lp")
     private String reflect_lp;
+
     @Column(name="isPublic")
     private Boolean isPublic;
+
+    @Range(min = 1, max = 5)
     @Column(name="rating")
     private Long rating;
 
