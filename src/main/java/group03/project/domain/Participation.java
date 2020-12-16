37af03b165d65f0ed0e12fe79980103d883ff0ad
Matt <barnettmd@cardiff.ac.uk>
2020-12-16 23:37:18 +0000

    Added validation for part. class

--- src/main/java/group03/project/domain/Participation.java
+++ src/main/java/group03/project/domain/Participation.java
@@ -3,6 +3,7 @@ package group03.project.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -16,16 +17,23 @@ public class Participation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="participationID")
+    @NonNull
+    @Column(name="participationID", nullable = false)
     private Long participationID;
-    @Column(name="Activity_activityID")
+
+    @NonNull
+    @Column(name="Activity_activityID", nullable = false)
     private Long activityID;
+
     @DateTimeFormat
     @Column(name="date")
     private Date date;
+
     @Column(name="Role_roleID")
     private String roleID;
-    @Column(name="siteUser_userID")
+
+    @NonNull
+    @Column(name="siteUser_userID", nullable = false)
     private Long userID;
 
 }
