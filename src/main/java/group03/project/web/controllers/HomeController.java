2c6b080f59a02b3d030345322209eb0220c6a87e
c1936922 <whitenj2@cardiff.ac.uk>
2020-12-16 17:03:06 +0000

    Small punctual changes made to keep consistency

--- src/main/java/group03/project/web/controllers/HomeController.java
+++ src/main/java/group03/project/web/controllers/HomeController.java
@@ -94,31 +94,23 @@ public class HomeController {
             //Make a list of all the tags that the current user has
             for (int partlist = 0; partlist < allParticipations.size(); partlist++) {
                 Participation participation = allParticipations.get(partlist);
-                System.out.println("current participation: " + participation);
                 if(participation.getUserID() == currentID) {
                     t_myParticipations.add(participation);
-                    System.out.println("All participations: "+ t_myParticipations);
                 }
             }
-            Integer counter =0;
-            Integer innerCounter = 0;
-            Integer deepcounter =0;
                 for (Participation mypart : t_myParticipations) {
                     t_myActivities.add(participationService.getRelatedActivity(mypart));
                 }
                 for (Activity myact : t_myActivities){
                         for (Objective obj : objService.getAllObjectives()) {
                             if (objService.getAssociatedActivity(obj) == myact) {
-                                //System.out.println("Objective " + obj.getObjectiveID() + " matches " + myact.getActivityID()
-                                // + ", objective tag " + obj.getTag().getTagName() + " being added to list of tags.");
+
                                 t_tagList.add(tagService.findATagByID(obj.getTag().getTagID()).get().getTagID());
-                                //System.out.println("Current tag list by id: " + t_tagList);
                             }
                         }
                     }
 
         List<Tag> allTags = tagService.findAllTags();
-        System.out.println("taglist: "+ t_tagList);
 
         Integer amountOfOfficialTags = tagService.findTagsIfOfficial().size();
 
@@ -140,7 +132,7 @@ public class HomeController {
         }
 
         model.addAttribute("userstags",tagNames);
-        model.addAttribute("uncompletedtags",uncompletedTagNames);
+        model.addAttribute("incompleteTags",uncompletedTagNames);
         model.addAttribute("tags", allTags);
         model.addAttribute("totaltagsamt",amountOfOfficialTags);
                 /*
